738b0a6db6f5dcd6a159b79438e97b538571bb10
Yisrael Gurkow <yisrelg3@gmail.com>
2021-05-21 16:53:44 -0400

    complete

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,16 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react'
+
+export default class CoordinatesButton extends Component {
+
+    coordinates = (event) => {
+        const xyArray = [event.clientX, event.clientY]
+        this.props.onReceiveCoordinates(xyArray)
+    }
+    
+  render() {
+    return (
+      <button onClick={this.coordinates}/>
+    )
+  }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,17 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react'
+
+export default class DelayedButton extends Component {
+
+    handleClick = (event)=> {
+        event.persist()
+        const xyArray = [event.clientX, event.clientY]
+        setTimeout(() => this.props.onDelayedClick(event), this.props.delay)
+    }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}/>
+    )
+  }
+}
